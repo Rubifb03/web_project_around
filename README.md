ff1667eabe63a78776d53bd4409413a45e958e4a
Rubi Floreano <rubifloreano912@gmail.com>
2024-11-18 12:55:48 -0500

    Correcciones sugeridas por el supervisor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tripleten web_project_around
+
+## Nombre del proyecto.
+
+Sprint 7: Alrededor de EEUU
+
+## Descripción del proyecto
+
+Página web responsiva sobre un perfil de Instagram. Se le dio vida al botón para que al momento de darle clic se abra un formaluario y poder editar los campos de nombre y descripción.
+
+## Plan de mejora
+
+Habilitar el botón Me Encanta.
--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ const profileName = document.querySelector(".profile__header-title");
 const profileJob = document.querySelector(".profile__description");
 const inputName = document.querySelector(".popup__input_type_name");
 const inputJob = document.querySelector(".popup__input_type_about");
+const saveButton = document.querySelector(".popup__submit-button");
 
 function toggleForm() {
   popup.classList.toggle("popup_visible");
@@ -14,18 +15,13 @@ function toggleForm() {
 openFormButton.addEventListener("click", toggleForm);
 closeButton.addEventListener("click", toggleForm);
 
-form.addEventListener("submit", handleFormSubmit);
+// form.addEventListener("submit", handleFormSubmit);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
+  toggleForm();
 }
 
 form.addEventListener("submit", handleFormSubmit);
-
-/*function toggleForm() {
-  inputName.value = profileName.textContent;
-  inputJob.value = profileJob.textContent;
-  popup.classList.add(".popup_visible");
-}*/
